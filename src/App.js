1eb7065627653a607860a3c5f6230b49a4fb31d1
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-06-17 17:48:03 -0300

    Add props in the components and add Sidebar

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__video">
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={100}
+          messages={200}
+          shares={300}
+          name="J.Alison"
+          description="Brecker o goleiro"
+          music="música épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video 
+          likes={100}
+          messages={200}
+          shares={300}
+          name="J.Alison"
+          description="Brecker o goleiro"
+          music="música épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
       </div>
     </div>
   );
